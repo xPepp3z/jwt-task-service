6c6d1a3e1bda737fe39242f12c3d878ac49edbd4
xPepp3z <giuseppegravagno57@gmail.com>
2026-03-18 11:46:28 +0100

    fix: rimosso il file .env da src e messo nel main

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,10 +1,9 @@
-import { type Request, type Response, type NextFunction } from "express";
-import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "../config.js";
-import { body } from "express-validator";
+import { type Request, type Response, type NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../config.js';
+import { body } from 'express-validator';
 
-
-declare module "express-serve-static-core" {
+declare module 'express-serve-static-core' {
   interface Request {
     user?: {
       _id: string;
@@ -20,7 +19,7 @@ export const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
     const token = req.query.token as string;
 
     if (!token) {
-      return res.status(401).json({ message: "Nessun token fornito" });
+      return res.status(401).json({ message: 'Nessun token fornito' });
     }
 
     const decoded: any = jwt.verify(token, JWT_SECRET);
@@ -30,20 +29,23 @@ export const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
 
     next();
   } catch (err) {
-    return res.status(401).json({ message: "Token non valido " +err });
+    return res.status(401).json({ message: 'Token non valido ' + err });
   }
 };
 
 export const validateRegister = [
-    body("email").isEmail().withMessage("Devi inserire una mail valida"),
-    body("password").isLength({ min: 6 }).withMessage("La password deve essere minimo di 6 caratteri"),
-    body("username").notEmpty().withMessage("Il campo username non può essere vuoto")
-  ]
-
-
+  body('email').isEmail().withMessage('Devi inserire una mail valida'),
+  body('password')
+    .isLength({ min: 6 })
+    .withMessage('La password deve essere minimo di 6 caratteri'),
+  body('username')
+    .notEmpty()
+    .withMessage('Il campo username non può essere vuoto'),
+];
 
 export const validateLogin = [
-    body("email").isEmail().withMessage("Devi inserire una mail valida"),
-    body("password").isLength({ min: 6 }).withMessage("La password deve essere minimo di 6 caratteri"),
-  ]
-
+  body('email').isEmail().withMessage('Devi inserire una mail valida'),
+  body('password')
+    .isLength({ min: 6 })
+    .withMessage('La password deve essere minimo di 6 caratteri'),
+];
